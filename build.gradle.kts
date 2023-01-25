e306f86cb02694152cbbd2f011a42b271c8ae19c
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-01-25 19:28:38 +0100

    Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val mojangMapped = project.hasProperty("mojang-mapped") || System.getProperty("m
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.8.0"
     id("xyz.xenondevs.specialsource-gradle-plugin") version "1.0.0"
     id("xyz.xenondevs.string-remapper-gradle-plugin") version "1.0.0"
     id("xyz.xenondevs.nova.nova-gradle-plugin") version libs.versions.nova
